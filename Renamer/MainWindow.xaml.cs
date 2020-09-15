fc2e199e45a4dcd317c75f6e2d7fc25c5db1bb8f
Andrei Vaida <andrei_vd2006@yahoo.com>
2020-09-15 21:38:36 +0300

    Fix bugs and add 'Delete duplicates' button

--- Renamer/MainWindow.xaml.cs
+++ Renamer/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System;
+using Microsoft.VisualBasic.FileIO;
 
 namespace Renamer
 {
@@ -10,41 +12,103 @@ namespace Renamer
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly string _currentFolderPath = Directory.GetCurrentDirectory();
+        private readonly string _workingFolderPath = Directory.GetCurrentDirectory();
+        private HashSet<string> _foldersWithConflicts;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void OnRenameClick(object sender, RoutedEventArgs e)
+        private void OnRenameClick(object sender, RoutedEventArgs e) => Rename();
+
+        private void Rename()
         {
+            _foldersWithConflicts = new HashSet<string>();
+            var renamed = 0;
+
             var fromSeparator = fromSeparatorTextBox.Text[0];
             var toSeparator = toSeparatorTextBox.Text[0];
-
             var folders = GetFoldersToRename(fromSeparator);
-            RenameFolders(folders, fromSeparator, toSeparator);
+            
+            foreach (var folderName in folders)
+            {
+                try
+                {
+                    RenameFolderOrMoveFiles(folderName, fromSeparator, toSeparator);
+                    renamed++;
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show($"Cannot rename '{folderName}'.\n{exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
 
-            executionMessage.Content = $"Renamed {folders.Count} folders";
+            executionMessage.Content = $"Renamed {renamed} folders";
+
+            if (_foldersWithConflicts.Count > 0 && deleteDuplicatesCheckBox.IsChecked == false)
+                AllertFoldersWithConflicts();
         }
 
         private HashSet<string> GetFoldersToRename(char fromSeparator) =>
-            Directory.GetDirectories(_currentFolderPath)
+            Directory.GetDirectories(_workingFolderPath)
                      .Select(Path.GetFileName)
                      .Where(name => name.Contains(fromSeparator))
                      .ToHashSet();
 
-        private void RenameFolders(HashSet<string> folders, char fromSeparator, char toSeparator)
+        private void RenameFolderOrMoveFiles(string folderName, char fromSeparator, char toSeparator)
         {
-            foreach (var folderName in folders)
-            {
-                var oldName = folderName.Split(fromSeparator);
-                var newName = oldName[2] + toSeparator + oldName[1] + toSeparator + oldName[0];
+            var oldFolderNameParts = folderName.Split(fromSeparator);
+            var newFolderName = oldFolderNameParts[2] + toSeparator + oldFolderNameParts[1] + toSeparator + oldFolderNameParts[0];
 
-                var currentPath = Path.Combine(_currentFolderPath, folderName);
-                var newPath = Path.Combine(_currentFolderPath, newName);
-                Directory.Move(currentPath, newPath);
+            var oldFolderPath = Path.Combine(_workingFolderPath, folderName);
+            var newFolderPath = Path.Combine(_workingFolderPath, newFolderName);
+
+            if (!Directory.Exists(newFolderPath))
+                RenameFolder(oldFolderPath, newFolderPath);
+            else
+            {
+                MoveFiles(oldFolderPath, newFolderPath);
+                DeleteFolderSafely(oldFolderPath);
             }
         }
+
+        private static void RenameFolder(string oldFolderPath, string newFolderPath) =>
+            Directory.Move(oldFolderPath, newFolderPath);
+
+        /// <summary>
+        /// Move the files to the existing folder, without overriding them.
+        /// </summary>
+        /// <param name="fromFolderPath">The folder from which all files will be moved</param>
+        /// <param name="toFolderPath">The folder to which all files will be moved</param>
+        private void MoveFiles(string fromFolderPath, string toFolderPath)
+        {
+            var filesToMove = new DirectoryInfo(fromFolderPath).GetFiles();
+            foreach (var filePath in filesToMove)
+                if (!File.Exists(Path.Combine(toFolderPath, filePath.Name)))
+                    File.Move(filePath.FullName, Path.Combine(toFolderPath, filePath.Name));
+                else
+                    _foldersWithConflicts.Add(Path.GetFileName(fromFolderPath));
+        }
+
+        /// <summary>
+        /// Move the old folder to Recycle Bin if is empty or if DeleteDuplicatesCheckBox is checked.
+        /// </summary>
+        /// <param name="folderPath"></param>
+        private void DeleteFolderSafely(string folderPath)
+        {
+            var noOfFiles = new DirectoryInfo(folderPath).GetFiles().Count();
+            if (noOfFiles == 0 || deleteDuplicatesCheckBox.IsChecked == true)
+                MoveFolderToRecycleBin(folderPath);
+        }
+
+        private static void MoveFolderToRecycleBin(string oldFolderPath) => 
+            FileSystem.DeleteDirectory(oldFolderPath,
+                                       UIOption.OnlyErrorDialogs,
+                                       RecycleOption.SendToRecycleBin,
+                                       UICancelOption.ThrowException);
+
+        private void AllertFoldersWithConflicts() => 
+            MessageBox.Show($"There were some conflicts in the following folders:\n\n{string.Join("\n", _foldersWithConflicts)}", "Info");
     }
 }
