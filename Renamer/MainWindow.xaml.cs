fe0ccd4ef17ce952474e8e44b63cc74bd5c8ef17
Andrei <andrei_vd2006@yahoo.com>
2022-08-06 16:43:59 +0300

    Rename Canon and Sony HDR CX405 file names to time names: YYYMMDD_HHMMSS

--- Renamer/MainWindow.xaml.cs
+++ Renamer/MainWindow.xaml.cs
@@ -11,14 +11,18 @@ namespace Renamer
     {
         private readonly string _workingFolderPath = Directory.GetCurrentDirectory();
         private readonly FolderService _folderService;
+        private readonly FileService _fileService;
 
         public MainWindow()
         {
             InitializeComponent();
             _folderService = new FolderService(_workingFolderPath, deleteDuplicatesCheckBox);
+            _fileService = new FileService(_workingFolderPath, executionMessage);
         }
 
         private void OnRenameFoldersClick(object sender, RoutedEventArgs e) => RenameFolders();
+
+        private void OnRenameFilesClick(object sender, RoutedEventArgs e) => RenameFiles();
 
         private void RenameFolders()
         {
@@ -26,5 +30,7 @@ namespace Renamer
             var toSeparator = toSeparatorTextBox.Text[0];
             _folderService.RenameFolders(fromSeparator, toSeparator, executionMessage);
         }
+
+        private void RenameFiles() => _fileService.RenameFilesNameToTimeName();
     }
 }
---
+++ Renamer/Services/FileService.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Directory = System.IO.Directory;
+using Image = System.Drawing.Image;
+using System.Drawing.Imaging;
+
+namespace Renamer.Services
+{
+    public class FileService
+    {
+        private readonly string _workingFolderPath;
+        private readonly Label _executionMessage;
+        private const string ShortcutSuffix = " - Shortcut.lnk";
+        private const int DateLenght = 8;
+        private const int TimeLenght = 6; // 20220806_144753
+        private static readonly Regex colonRegex = new Regex(":");
+
+        public FileService(string workingFolderPath, Label executionMessage)
+        {
+            _workingFolderPath = workingFolderPath;
+            this._executionMessage = executionMessage;
+        }
+
+        public void RenameFilesNameToTimeName()
+        {
+            var sonyFilesCount = 0;
+            var canonFilesCount = 0;
+            var unknownFiles = new List<string>();
+
+            var files = GetNonSmartphoneFiles();
+
+            foreach (var fileName in files)
+            {
+                try
+                {
+                    if (IsSonyHDRCX405File(fileName))
+                    {
+                        RenameSonyFile(fileName);
+                        sonyFilesCount++;
+                    }
+                    else if (IsCanonFile(fileName))
+                    {
+                        RenameCanonFile(fileName);
+                        canonFilesCount++;
+                    }
+                    else
+                    {
+                        unknownFiles.Add(fileName);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show($"Cannot rename '{fileName}'.\n{exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            _executionMessage.Content = $"Renamed: {sonyFilesCount} Sony, {canonFilesCount} Canon. Found {unknownFiles.Count} unknown: {string.Join(", ", unknownFiles)}";
+        }
+
+        private List<string> GetNonSmartphoneFiles() =>
+            Directory.GetFiles(_workingFolderPath)
+                     .Select(Path.GetFileName)
+                     .Where(name => !IsSmartphoneFile(name))
+                     .Select(RemoveShortcutSuffix)
+                     .ToList();
+
+        private bool IsSmartphoneFile(string name)
+        {
+            if (!name.Contains('_'))
+                return false;
+
+            var parts = name.Split('_');
+            return parts[0].Length == DateLenght && parts[1].Length >= TimeLenght;
+        }
+
+        private string RemoveShortcutSuffix(string name)
+        {
+            if (name.EndsWith(ShortcutSuffix))
+                return name.Remove(name.Length - ShortcutSuffix.Length);
+
+            return name;
+        }
+
+        private bool IsSonyHDRCX405File(string fileName) => fileName.All(char.IsDigit);
+        private bool IsCanonFile(string fileName) => fileName.StartsWith("IMG_");
+        private bool IsSonyA6600File(string fileName) => fileName.StartsWith("DSC") || fileName.StartsWith("C"); // Date taken + Date modified
+
+        private void RenameSonyFile(string fileName)
+        {
+            var newName = fileName.Insert(DateLenght, "_") + " - Sony";
+            RenameShortcut(fileName, newName);
+        }
+
+        private void RenameCanonFile(string fileName)
+        {
+            var originalFilePath = GetFilePathFromShortcut(fileName + ShortcutSuffix);
+            var dateTime = GetDateTakenFromImage(originalFilePath);
+
+            var newName = $"{dateTime.Year}{dateTime.Month:D2}{dateTime.Day:D2}_{dateTime.Hour:D2}{dateTime.Minute:D2}{dateTime.Second:D2} - {fileName}";
+            RenameShortcut(fileName, newName);
+        }
+
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                PropertyItem propItem = myImage.GetPropertyItem(36867);
+                string dateTaken = colonRegex.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                return DateTime.Parse(dateTaken);
+            }
+        }
+
+        private void RenameShortcut(string fileName, string newName)
+        {
+            fileName += ShortcutSuffix;
+            newName += ".lnk";
+            File.Move(fileName, newName);
+        }
+
+        private string GetFilePathFromShortcut(string fileName)
+        {
+            IWshRuntimeLibrary.WshShell shell = new IWshRuntimeLibrary.WshShell();
+            IWshRuntimeLibrary.IWshShortcut link = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(fileName);
+            return link.TargetPath;
+        }
+    }
+}
